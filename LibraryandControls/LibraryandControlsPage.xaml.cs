de76225fa523a094f7355ecdec010bd49405b0c1
Dragos\Methoda <methoda23@gmail.com>
2016-07-02 15:05:02 +0300

    First commit

---
+++ LibraryandControls/LibraryandControlsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LibraryandControls
+{
+	public partial class LibraryandControlsPage : ContentPage
+	{
+		public LibraryandControlsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
